48c222a57e0891ec2be80085bdfb3a7e13a4e81b
Brandon Alfaro H. <brandonandreyalfaro@gmail.com>
2025-03-11 23:03:23 -0600

    Adding code to K-Nearest Neighbors.py.
    Signed-off-by: Brandon Alfaro H. <brandonandreyalfaro@gmail.com>

--- src/2.Classification/1.K-Nearest_Neighbors/k_nearest_neighbors.py
+++ src/2.Classification/1.K-Nearest_Neighbors/k_nearest_neighbors.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
 df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv')
 df.head()
@@ -18,3 +21,21 @@ X[0:5]
 
 y = df['custcat'].values
 y[0:5]
+
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+X[0:5]
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+k = 4
+#Train Model and Predict  
+neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+neigh
+
+yhat = neigh.predict(X_test)
+yhat[0:5]
+
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
