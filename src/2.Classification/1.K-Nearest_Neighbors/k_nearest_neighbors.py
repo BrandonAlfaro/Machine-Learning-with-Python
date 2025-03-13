d62b0694e4a742fd467da670ab7f1dd7748a74c9
BrandonAlfaro <brandonandreyalfaro@gmail.com>
2025-03-13 17:05:38 -0600

    Modifications to code.
    Signed-off-by: BrandonAlfaro <brandonandreyalfaro@gmail.com>

--- src/2.Classification/1.K-Nearest_Neighbors/k_nearest_neighbors.py
+++ src/2.Classification/1.K-Nearest_Neighbors/k_nearest_neighbors.py
@@ -1,10 +1,39 @@
-import numpy as np  # Import NumPy for numerical operations
-import matplotlib.pyplot as plt  # Import Matplotlib for data visualization
-import pandas as pd  # Import Pandas for data manipulation
-from sklearn import preprocessing  # Import preprocessing tools from scikit-learn
-from sklearn.model_selection import train_test_split  # Import function to split data into training and test sets
-from sklearn.neighbors import KNeighborsClassifier  # Import KNN classifier
-from sklearn import metrics  # Import metrics to evaluate model performance
+# ------------------------------------------------------------------------------
+# File: s_linear_regressiok_nearest_neigbors.py
+# Description: Implementation of a basic linear regression model in Python.
+#
+# Author: BrandonAlfaro
+# Date: 27/1/2025
+#
+# License: MIT
+# Copyright (c) 2025 BrandonAlfaro
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import numpy as np                                      # Import NumPy for numerical operations
+import matplotlib.pyplot as plt                         # Import Matplotlib for data visualization
+import pandas as pd                                     # Import Pandas for data manipulation
+from sklearn import preprocessing                       # Import preprocessing tools from scikit-learn
+from sklearn.model_selection import train_test_split    # Import function to split data into training and test sets
+from sklearn.neighbors import KNeighborsClassifier      # Import KNN classifier
+from sklearn import metrics                             # Import metrics to evaluate model performance
 
 # Load dataset from a URL into a Pandas DataFrame
 df = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv')
@@ -49,3 +78,6 @@ yhat[0:5]  # Display the first 5 predictions
 # Evaluate model accuracy on training and test sets
 print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
 print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+
+
