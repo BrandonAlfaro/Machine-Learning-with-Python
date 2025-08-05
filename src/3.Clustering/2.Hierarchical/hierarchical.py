8c2724920a8a1cba8e71b86e473f4cc18c6abbdd
BrandonAlfaro <brandonandreyalfaro@gmail.com>
2025-08-04 20:31:35 -0600

    Data blobs and agglomerative clustering made.
    Signed-off-by: BrandonAlfaro <brandonandreyalfaro@gmail.com>

--- src/3.Clustering/2.Hierarchical/hierarchical.py
+++ src/3.Clustering/2.Hierarchical/hierarchical.py
@@ -38,4 +38,46 @@ from matplotlib import pyplot as plt
 from sklearn import manifold, datasets 
 from sklearn.cluster import AgglomerativeClustering 
 from sklearn.datasets import make_blobs 
+from sklearn.preprocessing import MinMaxScaler
 #---                                        ---#
+
+#---         Generating Random Data         ---#
+X1, y1 = make_blobs(n_samples=50, centers=[[4,4], [-2, -1], [1, 1], [10,4]], cluster_std=0.9)
+plt.scatter(X1[:, 0], X1[:, 1], marker='o')
+plt.show()
+#---                                        ---#
+
+#---        Agglomerative Clustering        ---#
+agglom = AgglomerativeClustering(n_clusters = 4, linkage = 'average')
+agglom.fit(X1,y1)
+#---                                        ---#
+
+
+#---   Create a figure with specific size   ---#
+plt.figure(figsize=(6,4))
+#---                                        ---#
+
+#---          Normalization of X1           ---#
+X1 = MinMaxScaler().fit_transform(X1)
+#---                                        ---#
+
+#---     Displays all of the datapoints     ---#
+for i in range(X1.shape[0]):
+    # Replace the data points with their respective cluster value 
+    # (ex. 0) and is color coded with a colormap (plt.cm.spectral)
+    plt.text(X1[i, 0], X1[i, 1], str(y1[i]),
+             color=plt.cm.nipy_spectral(agglom.labels_[i] / 10.),
+             fontdict={'weight': 'bold', 'size': 9})
+
+# Remove the x ticks, y ticks, x and y axis
+plt.xticks([])
+plt.yticks([])
+
+# Display the plot of the original data before clustering
+plt.scatter(X1[:, 0], X1[:, 1], marker='.')
+
+# Display the plot
+plt.show()
+#---                                        ---#
+
+
